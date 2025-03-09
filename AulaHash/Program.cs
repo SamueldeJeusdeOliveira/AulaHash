dfb3e4bf01d818e771bd7b43712500bd37ace798
SamuelDeJesusDeOliveira <samuksjj@gmail.com>
2025-03-09 14:22:00 -0300

    Adicionar arquivos de projeto.

---
+++ AulaHash/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Maria");
+            set.Add("Ales");
+
+            Console.WriteLine(set.Contains("Maria"));
+        }
+    }
+}
