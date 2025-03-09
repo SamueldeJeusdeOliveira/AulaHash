dc97d09582cbb0bd529f43d4891e76d8e3bf04ab
SamuelDeJesusDeOliveira <samuksjj@gmail.com>
2025-03-09 14:39:40 -0300

    commit2

--- AulaHash/Entities/Product.cs
+++ AulaHash/Entities/Product.cs
@@ -9,5 +9,18 @@
             Name = name;
             Price = price;
         }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Product))
+            {
+                return false;
+            }
+            Product other = obj as Product;
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
     }
 }
--- AulaHash/Program.cs
+++ AulaHash/Program.cs
@@ -1,14 +1,24 @@
-﻿namespace Course
+﻿using Course.Entities;
+
+namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
-            set.Add("Maria");
-            set.Add("Ales");
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("Tv", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
 
-            Console.WriteLine(set.Contains("Maria"));
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine(a.Contains(prod));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
         }
     }
 }
